1bcc4a4a8975a0713d3577893ac4cc0c6ac98f63
ROJON Loic UPR SE/IRM <PTCX1319@orange.com>
2019-11-05 17:40:17 +0100

    Tentative d'ajout de TabBar

---
+++ classes/dao.php
@@ -0,0 +1,19 @@
+﻿<?php
+
+class dao{
+
+    public $db;
+    /*private static $instance;*/
+
+    public function __construct()
+    {
+        $this->db = new PDO("mysql:dbname=bestiary;host=localhost","root","");
+    }
+/*
+    public getInstance()
+    {
+        if($this->instance == null)
+            $this->instance = new dao();
+        return $this->instance;
+    }*/
+}
