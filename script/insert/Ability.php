b5a7f20f05c2024fd870508c4c276dd3f713d537
ROJON Loic UPR SE/IRM <PTCX1319@orange.com>
2019-11-18 17:57:48 +0100

    V2.1 - Ajout de état Tab & des interaction. Modif bdd pour l'ajout

--- script/insert/Ability.php
+++ script/insert/Ability.php
@@ -5,10 +5,15 @@ require_once '../include.php';
 if(isset($_POST['name']))
 {
     $dao = new dao();
+    $healths = $dao->db->query("SELECT * FROM health")->fetchAll();
+    $effect = $_POST['effect'];
+    foreach ($healths as $value) {
+        $effect = str_replace($value['name'], "<button class='btn btn-link' onclick='goHealth(".$value['id'].")'>".$value['name']."</button>", $effect);
+    }
     $res = $dao->db->prepare("INSERT INTO ability(`name`,`limit`,effect) VALUES(:name, :limit, :effect)")->execute(array(
         ':name' => $_POST['name'],
         ':limit' => ($_POST['limit'] == "true" ? 1: 0),
-        ':effect' => $_POST['effect']
+        ':effect' => $effect
     ));
 }
 else
--- script/insert/Skill.php
+++ script/insert/Skill.php
@@ -21,6 +21,11 @@ if(count($_POST) > 0)
         if(empty($res))
         {
             $effect = str_replace("'","\'",$_POST['effect']);
+            $healths = $dao->db->query("SELECT * FROM health")->fetchAll();
+            foreach ($healths as $value) {
+                echo $effect."<br>";
+                $effect = str_replace($value['name'], "<button class=\'btn btn-link\' onclick=\'goHealth(".$value['id'].")\'>".$value['name']."</button>",$effect);
+            }
 
             $dao->db->exec("INSERT INTO skill(`name`,path,rank,`limit`,effect) VALUES('".$_POST['name']."','".$_POST['path']."',".$_POST['rank'].",".($_POST['limit'] == 'true' ? 1 : 0).",'".$effect."')");
         }
